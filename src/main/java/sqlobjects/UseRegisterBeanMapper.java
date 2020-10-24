602ceedb3cb51524950badf7c29af45b79f23afb
hao <hao.deng.m@gmail.com>
2020-10-24 14:27:56 +0200

    Add UseRegisterBeanMapper prefix attribute case

--- src/main/java/sqlobjects/UseRegisterBeanMapper.java
+++ src/main/java/sqlobjects/UseRegisterBeanMapper.java
@@ -1,6 +1,5 @@
 package sqlobjects;
 
-import bean.UserBean;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
@@ -9,18 +8,81 @@ import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.util.List;
+import java.util.Map;
 
 public class UseRegisterBeanMapper {
     public interface UserDao {
-        @SqlUpdate("insert into users (id, name) values (?, ?)")
-        int insert(long id, String name);
+        @SqlUpdate("insert into users (id, name, role_id) values (?, ?, ?)")
+        int insert(long id, String name, long roleId);
 
-        @SqlUpdate("create table users (id int primary key, name varchar(100))")
+        @SqlUpdate("create table users (id int primary key, name varchar(100), role_id int)")
         void createTable();
 
         @SqlQuery("select * from users")
-        @RegisterBeanMapper(UserBean.class)
-        List<UserBean> list();
+        @RegisterBeanMapper(User.class)
+        List<User> list();
+
+        @SqlQuery("select u.id u_id, u.name u_name, r.id r_id, r.name r_name " +
+                "from users u left join roles r on u.role_id = r.id")
+        @RegisterBeanMapper(value = User.class, prefix = "u")
+        @RegisterBeanMapper(value = Role.class, prefix = "r")
+        Map<User,Role> getRolesPerUser();
+    }
+
+    public static class User {
+        private int id;
+        private String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "(User id:" + id +
+                    ", name:" + name + ")";
+        }
+    }
+
+    public static class Role {
+        private int id;
+        private String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "(Role id:" + id +
+                    ", name:" + name + ")";
+        }
     }
 
     public void testBeanMapper()
@@ -32,14 +94,36 @@ public class UseRegisterBeanMapper {
         UserDao userDao = handle.attach(UserDao.class);
 
         userDao.createTable();
-        userDao.insert(1L, "Bob");
-        userDao.insert(2L, "Kim");
+        userDao.insert(1L, "Bob", 1);
+        userDao.insert(2L, "Kim", 2);
 
         userDao.list().forEach(user -> System.out.println(user));
+    }
+
+    public void testPrefixBeanMapper()
+    {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test_testPrefixBeanMapper");
+        jdbi.installPlugin(new SqlObjectPlugin());
+
+        Handle handle = jdbi.open();
+        UserDao userDao = handle.attach(UserDao.class);
+
+        userDao.createTable();
+        userDao.insert(1L, "Bob", 1);
+        userDao.insert(2L, "Kim", 2);
+
+        handle.execute("create table roles (id int primary key, name varchar(100))");
+        handle.execute("insert into roles (id, name) values (1, 'guest')");
+        handle.execute("insert into roles (id, name) values (2, 'admin')");
+
+        Map<User, Role> userRoleMap = userDao.getRolesPerUser();
+        userRoleMap.entrySet()
+                .forEach(entry -> System.out.println(entry.getKey() + ", " + entry.getValue()));
     }
 
     public static void main(String[] args) {
         UseRegisterBeanMapper mapper = new UseRegisterBeanMapper();
         mapper.testBeanMapper();
+        mapper.testPrefixBeanMapper();
     }
 }
